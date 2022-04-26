35b9296e3ae938f87a4a8e8f2288951c555a38bb
Jessica Fatta <jessicafatta@gmail.com>
2022-04-26 11:22:03 -0400

    Complete api calls for get request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,7 @@
-export const getUrls = () => {
+const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
+      .then(data => data.urls)
 }
+
+export {getUrls }
